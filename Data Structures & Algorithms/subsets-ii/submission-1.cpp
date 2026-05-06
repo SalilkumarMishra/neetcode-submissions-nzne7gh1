d7f82d930d60bdefa16bd841b32421f831f4dd78
SalilkumarMishra <147610541+SalilkumarMishra@users.noreply.github.com>
2026-05-07 01:19:15 +0530

    Add: subsets-ii - submission-1

---
+++ Data Structures & Algorithms/subsets-ii/submission-1.cpp
@@ -0,0 +1,25 @@
+
+class Solution {
+public:
+    vector<vector<int>> result;
+
+    void backtrack(int start, vector<int>& nums, vector<int>& temp) {
+        result.push_back(temp);
+
+        for (int i = start; i < nums.size(); i++) {
+            // skip duplicates
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            temp.push_back(nums[i]);
+            backtrack(i + 1, nums, temp);
+            temp.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); // important
+        vector<int> temp;
+        backtrack(0, nums, temp);
+        return result;
+    }
+};
